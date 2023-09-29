9ecbc54c36e09abdafde5ef479d19581242b685f
Pedja Jevtic <designbyheart@gmail.com>
2023-09-29 11:03:54 +0200

    Added warriors excersize

---
+++ Warriors&Wizards/Warriors&Wizards/Character.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warriors_Wizards
+{
+    public  abstract class Character
+    {
+        protected bool IsVulnerarble;
+
+        public abstract string ToString();
+        public abstract void DamagePoints(Character target);
+
+        public bool Vulnerable() {
+            return this.IsVulnerarble;
+        }
+        public Character() { 
+            IsVulnerarble = true;
+        }
+    }
+
+    public class Warrior : Character
+    {
+        public override void DamagePoints(Character target)
+        {
+            Console.WriteLine("Warrior damage points: {0} {1}", target.ToString(), target.Vulnerable()  ? 10 : 6);
+        }
+
+        public override string ToString()
+        {
+            return "Warrior";
+        }
+
+        public Warrior()
+        {
+            this.IsVulnerarble = false;
+        }
+    }
+
+    public class Wizard : Character
+    {
+        protected bool IsSpellPrepared = false;
+        public override void DamagePoints(Character target)
+        {
+            Console.WriteLine("damage points {0} {1}", target.ToString(), this.IsSpellPrepared ? 12 : 3);
+            this.IsVulnerarble = true;
+        }
+
+        public override string ToString()
+        {
+            return "Wizard";
+        }
+
+        public void PrepareSpell() {
+            this.IsVulnerarble = false;
+            this.IsSpellPrepared = true;
+        }
+
+        public Wizard()
+        {
+            this.IsVulnerarble = true;
+        }
+    }
+}
---
+++ Warriors&Wizards/Warriors&Wizards/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Warriors_Wizards;
+
+namespace WarriorsAndWizards {
+    class Program {
+        public static void Main(string[] args)
+        {
+            var warior = new Warrior();
+            var wizard = new Wizard();
+
+            wizard.DamagePoints(warior);
+
+            wizard.PrepareSpell();
+            wizard.DamagePoints(warior);
+            warior.DamagePoints(wizard);
+        }
+    }
+
+}
